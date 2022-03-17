7bc4e22ead7f440c43819eca5e068fa988f39e08
Thiago Sartorel <thiagosartorel@uniplac.net>
2022-03-17 14:10:05 -0300

    atualização

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         zzzzuuuuuuuuuuuuuuuuunipprac
+         zzzzuuuuuuuuuuuuuuuunipprac
         </h1>
 
       </main>
